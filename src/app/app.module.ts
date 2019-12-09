b886f40f91cc0f50f2a9ca3a36a3ecde6e01df21
skeres <stephane.keresztes@gmail.com>
2019-12-09 18:46:05 +0100

    mise en place partiel de ngrx. A compléter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,46 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+
+import {HttpClientModule} from '@angular/common/http';
+    
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { ShopReducer } from './store/reducer';
+import { EffectsModule } from '@ngrx/effects';
+import { ShopEffects } from './store/effects';
+import { ProductComponent } from './product/product.component';
+import { ProductListComponent } from './product-list/product-list.component';
+    
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    ProductComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot(reducers, {
+      metaReducers, 
+      runtimeChecks: {
+        strictStateImmutability: true,
+        strictActionImmutability: true,
+      }
+    }),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    StoreRouterConnectingModule.forRoot({stateKey:'router'}),
+    HttpClientModule,
+    StoreModule.forRoot({ shop: ShopReducer }),
+    EffectsModule.forRoot([ShopEffects])
   ],
   providers: [],
   bootstrap: [AppComponent]
